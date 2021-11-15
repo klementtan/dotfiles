097d844b6dc8b934bff7b8c455d189a1de9bc175
klementtan <klementtan@gmail.com>
2021-11-15 13:31:28 +0800

    Add setup scripts

---
+++ scripts/macos.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Installing xcode"
+xcode-select —-install
+echo "Installing homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Brew installing"
+xargs brew install < brew.txt
--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
+#!/bin/sh
 
-ZSHRC_FILE=~/.zshrc
-CONFIG_DIR=~/.config
-
-if test -f "$ZSHRC_FILE"; then
-  echo "Moving $ZSHRC_FILE to ${ZSHRC_FILE}.bak"
-  mv $ZSHRC_FILE ${ZSHRC_FILE}.bak
-fi
-
-if test -f "$CONFIG_DIR"; then
-  echo "Moving $CONFIG_DIR to ${CONFIG_DIR}_bak"
-  mv $CONFIG_DIR ${CONFIG_DIR}.bak
-fi
+echo "Setting up zsh"
+./scripts/zsh.sh
