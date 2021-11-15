e0d7f77afa7a5bb9baf310aca1a4a594b8c77478
klementtan <klementtan@gmail.com>
2021-11-15 13:13:49 +0800

    Fix startup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 
-ZSHRC_FILE=~/.zshrc_tmp
+ZSHRC_FILE=~/.zshrc
+CONFIG_DIR=~/.config
 
 if test -f "$ZSHRC_FILE"; then
   echo "Moving $ZSHRC_FILE to ${ZSHRC_FILE}.bak"
   mv $ZSHRC_FILE ${ZSHRC_FILE}.bak
+fi
+
+if test -f "$CONFIG_DIR"; then
+  echo "Moving $CONFIG_DIR to ${CONFIG_DIR}_bak"
+  mv $CONFIG_DIR ${CONFIG_DIR}.bak
 fi
